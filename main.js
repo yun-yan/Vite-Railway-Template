9717dcd081a44abff0f55b35dd72edfe86492d3c
unknown <d187108@gmail.com>
2023-03-20 02:05:45 +1100

    try js

--- main.js
+++ main.js
@@ -1,228 +1,208 @@
-    gsap.registerPlugin(ScrollTrigger);
-    let speed = 100;
-
-    /*  SCENE 1 */
-    let scene1 = gsap.timeline();
-    ScrollTrigger.create({
-        animation: scene1,
-        trigger: ".scrollElement",
-        start: "top top",
-        end: "45% 100%",
-        scrub: 3,
-    });
-
-    // hills animation 
-    scene1.to("#h1-1", { y: 3 * speed, x: 1 * speed, scale: 0.9, ease: "power1.in" }, 0)
-    scene1.to("#h1-2", { y: 2.6 * speed, x: -0.6 * speed, ease: "power1.in" }, 0)
-    scene1.to("#h1-3", { y: 1.7 * speed, x: 1.2 * speed }, 0.03)
-    scene1.to("#h1-4", { y: 3 * speed, x: 1 * speed }, 0.03)
-    scene1.to("#h1-5", { y: 2 * speed, x: 1 * speed }, 0.03)
-    scene1.to("#h1-6", { y: 2.3 * speed, x: -2.5 * speed }, 0)
-    scene1.to("#h1-7", { y: 5 * speed, x: 1.6 * speed }, 0)
-    scene1.to("#h1-8", { y: 3.5 * speed, x: 0.2 * speed }, 0)
-    scene1.to("#h1-9", { y: 3.5 * speed, x: -0.2 * speed }, 0)
-
-    //animate text
-    scene1.to("#info", { y: 8 * speed }, 0)
+const ee = new EventEmitter();
+const App = React.createClass({ displayName: "App",
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("main", { className: "react-calculator" }, /*#__PURE__*/
+      React.createElement(InputField, null), /*#__PURE__*/
+      React.createElement(TotalRecall, null), /*#__PURE__*/
+      React.createElement(ButtonSetNumbers, null), /*#__PURE__*/
+      React.createElement(ButtonSetFunctions, null), /*#__PURE__*/
+      React.createElement(ButtonSetEquations, null)));
 
 
+  } });
 
-    /*   Bird   */
-    gsap.fromTo("#bird", { opacity: 1 }, {
-        y: -250,
-        x: 800,
-        ease: "power2.out",
-        scrollTrigger: {
-            trigger: ".scrollElement",
-            start: "15% top",
-            end: "60% 100%",
-            scrub: 4,
-            onEnter: function() { gsap.to("#bird", { scaleX: 1, rotation: 0 }) },
-            onLeave: function() { gsap.to("#bird", { scaleX: -1, rotation: -15 }) },
-        }
-    })
+const Button = React.createClass({ displayName: "Button",
+  _handleClick() {
+    let text = this.props.text,
+    cb = this.props.clickHandler;
 
+    if (cb) {
+      cb.call(null, text);
+    }
+  },
 
-    /* Clouds  */
-    let clouds = gsap.timeline();
-    ScrollTrigger.create({
-        animation: clouds,
-        trigger: ".scrollElement",
-        start: "top top",
-        end: "70% 100%",
-        scrub: 1,
-    });
-
-    clouds.to("#cloud1", { x: 500 }, 0)
-    clouds.to("#cloud2", { x: 1000 }, 0)
-    clouds.to("#cloud3", { x: -1000 }, 0)
-    clouds.to("#cloud4", { x: -700, y: 25 }, 0)
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("button", { className: this.props.klass, onClick: this._handleClick }, /*#__PURE__*/
+      React.createElement("span", { className: "title" }, this.props.text)));
 
 
+  } });
 
-    /* Sun motion Animation  */
-    let sun = gsap.timeline();
-    ScrollTrigger.create({
-        animation: sun,
-        trigger: ".scrollElement",
-        start: "top top",
-        end: "2200 100%",
-        scrub: 1,
-    });
+const ContentEditable = React.createClass({ displayName: "ContentEditable",
+  _handleClick() {
+    const cb = this.props.clickHandler;
 
-    //sun motion 
-    sun.to("#bg_grad", { attr: { cy: "330" } }, 0.00)
+    if (cb) {
+      cb.call(this);
+    }
+  },
 
-    //bg change
-    sun.to("#sun", { attr: { offset: "0.15" } }, 0.00)
-    sun.to("#bg_grad stop:nth-child(2)", { attr: { offset: "0.15" } }, 0.00)
-    sun.to("#bg_grad stop:nth-child(3)", { attr: { offset: "0.18" } }, 0.00)
-    sun.to("#bg_grad stop:nth-child(4)", { attr: { offset: "0.25" } }, 0.00)
-    sun.to("#bg_grad stop:nth-child(5)", { attr: { offset: "0.46" } }, 0.00)
-    sun.to("#bg_grad stop:nth-child(6)", { attr: { "stop-color": "#FF9171" } }, 0)
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("div", { className: "editable-field", contentEditable: this.props.initEdit, spellcheck: this.props.spellCheck, onClick: this._handleClick },
+      this.props.text));
 
 
-
-    /*   SCENE 2  */
-    let scene2 = gsap.timeline();
-    ScrollTrigger.create({
-        animation: scene2,
-        trigger: ".scrollElement",
-        start: "15% top",
-        end: "40% 100%",
-        scrub: 4,
-    });
-
-    scene2.fromTo("#h2-1", { y: 500, opacity: 0 }, { y: 0, opacity: 1 }, 0)
-    scene2.fromTo("#h2-2", { y: 500 }, { y: 0 }, 0.1)
-    scene2.fromTo("#h2-3", { y: 700 }, { y: 0 }, 0.1)
-    scene2.fromTo("#h2-4", { y: 700 }, { y: 0 }, 0.2)
-    scene2.fromTo("#h2-5", { y: 800 }, { y: 0 }, 0.3)
-    scene2.fromTo("#h2-6", { y: 900 }, { y: 0 }, 0.3)
+  } });
 
 
+const InputField = React.createClass({ displayName: "InputField",
+  _updateField(newStr) {
+    newStr = newStr.split ? newStr.split(' ').reverse().join(' ') : newStr;
+    return this.setState({ text: newStr });
+  },
 
-    /* Bats */
-    gsap.fromTo("#bats", { opacity: 1, y: 400, scale: 0 }, {
-        y: 120,
-        scale: 0.8,
-        transformOrigin: "50% 50%",
-        ease: "power3.out",
-        scrollTrigger: {
-            trigger: ".scrollElement",
-            start: "40% top",
-            end: "70% 100%",
-            scrub: 3,
-            onEnter: function() {
-                gsap.utils.toArray("#bats path").forEach((item, i) => {
-                    gsap.to(item, { scaleX: 0.5, yoyo: true, repeat: 11, duration: 0.15, delay: 0.7 + (i / 10), transformOrigin: "50% 50%" })
-                });
-                gsap.set("#bats", { opacity: 1 })
-            },
-            onLeave: function() { gsap.to("#bats", { opacity: 0, delay: 2 }) },
-        }
-    })
+  getInitialState() {
+    this.props.text = this.props.text || '0';
 
+    return { text: this.props.text };
+  },
 
-    /* Sun increase */
-    let sun2 = gsap.timeline();
-    ScrollTrigger.create({
-        animation: sun2,
-        trigger: ".scrollElement",
-        start: "2200 top",
-        end: "5000 100%",
-        scrub: 1,
-    });
+  componentWillMount() {
+    ee.addListener('numberCruncher', this._updateField);
+  },
 
-    sun2.to("#sun", { attr: { offset: "0.6" } }, 0)
-    sun2.to("#bg_grad stop:nth-child(2)", { attr: { offset: "0.7" } }, 0)
-    sun2.to("#sun", { attr: { "stop-color": "#ffff00" } }, 0)
-    sun2.to("#lg4 stop:nth-child(1)", { attr: { "stop-color": "#623951" } }, 0)
-    sun2.to("#lg4 stop:nth-child(2)", { attr: { "stop-color": "#261F36" } }, 0)
-    sun2.to("#bg_grad stop:nth-child(6)", { attr: { "stop-color": "#45224A" } }, 0)
+  render() {
+    return /*#__PURE__*/React.createElement(ContentEditable, { text: this.state.text, initEdit: "false", spellcheck: "false", clickHandler: this._clickBait });
+  } });
 
+const TotalRecall = React.createClass({ displayName: "TotalRecall",
+  _toggleMemories() {
+    this.setState({ show: !this.state.show });
+  },
 
+  _recallMemory(memory) {
+    store.newInput = memory;
+    ee.emitEvent('toggle-memories');
+  },
 
-    /* Transition (from Scene2 to Scene3) */
-    gsap.set("#scene3", { y: 580, visibility: "visible" })
-    let sceneTransition = gsap.timeline();
-    ScrollTrigger.create({
-        animation: sceneTransition,
-        trigger: ".scrollElement",
-        start: "70% top",
-        end: "bottom 100%",
-        scrub: 3,
-    });
+  getInitialState() {
+    return { show: false };
+  },
 
-    sceneTransition.to("#h2-1", { y: -680, scale: 1.5, transformOrigin: "50% 50%" }, 0)
-    sceneTransition.to("#bg_grad", { attr: { cy: "-80" } }, 0.00)
-    sceneTransition.to("#bg2", { y: 0 }, 0)
+  componentWillMount() {
+    ee.addListener('toggle-memories', this._toggleMemories);
+  },
+
+  render() {
+    let classNames = `memory-bank ${this.state.show ? 'visible' : ''}`;
+
+    return /*#__PURE__*/(
+      React.createElement("section", { className: classNames }, /*#__PURE__*/
+      React.createElement(Button, { text: "+", clickHandler: this._toggleMemories, klass: "toggle-close" }),
+      store.curMemories.map(mem => {
+        return /*#__PURE__*/(
+          React.createElement(Button, { klass: "block memory transparent", text: mem, clickHandler: this._recallMemory }));
+
+      })));
 
 
+  } });
 
-    /* Scene 3 */
-    let scene3 = gsap.timeline();
-    ScrollTrigger.create({
-        animation: scene3,
-        trigger: ".scrollElement",
-        start: "80% 50%",
-        end: "bottom 100%",
-        scrub: 3,
-    });
+const ButtonSetFunctions = React.createClass({ displayName: "ButtonSetFunctions",
+  _showMemoryBank() {
+    ee.emitEvent('toggle-memories');
+  },
 
-    //Hills motion
-    scene3.fromTo("#h3-1", { y: 300 }, { y: -550 }, 0)
-    scene3.fromTo("#h3-2", { y: 800 }, { y: -550 }, 0.03)
-    scene3.fromTo("#h3-3", { y: 600 }, { y: -550 }, 0.06)
-    scene3.fromTo("#h3-4", { y: 800 }, { y: -550 }, 0.09)
-    scene3.fromTo("#h3-5", { y: 1000 }, { y: -550 }, 0.12)
+  _clear() {
+    store.newInput = 0;
+  },
 
-    //stars
-    scene3.fromTo("#stars", { opacity: 0 }, { opacity: 0.5, y: -500 }, 0)
+  _contentClear() {
+    let curInput = String(store.curInput),
+    lessOne = curInput.substring(0, curInput.length - 1);
 
-    // Scroll Back text
-    scene3.fromTo("#arrow2", { opacity: 0 }, { opacity: 0.7, y: -710 }, 0.25)
-    scene3.fromTo("#text2", { opacity: 0 }, { opacity: 0.7, y: -710 }, 0.3)
+    return store.newInput = lessOne === '' ? 0 : lessOne;
+  },
 
-    //gradient value change
-    scene3.to("#bg2-grad", { attr: { cy: 600 } }, 0)
-    scene3.to("#bg2-grad", { attr: { r: 500 } }, 0)
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("section", { className: "button-set--functions" }, /*#__PURE__*/
+      React.createElement(Button, { klass: "long-text", text: "recall", clickHandler: this._showMemoryBank }), /*#__PURE__*/
+      React.createElement(Button, { klass: "long-text", text: "clear", clickHandler: this._clear }), /*#__PURE__*/
+      React.createElement(Button, { text: "\u2190", clickHandler: this._contentClear })));
 
 
-    /*   falling star   */
-    gsap.to("#fstar", {
-        x: -700,
-        y: -250,
-        ease: "power4.out",
-        scrollTrigger: {
-            trigger: ".scrollElement",
-            start: "4000 top",
-            end: "6000 100%",
-            scrub: 5,
-            onEnter: function() { gsap.set("#fstar", { opacity: 1 }) },
-            onLeave: function() { gsap.set("#fstar", { opacity: 0 }) },
-        }
-    })
+  } });
+
+const ButtonSetEquations = React.createClass({ displayName: "ButtonSetEquations",
+  _eq(type) {
+    store.newInput = `${store.curInput} ${type} `;
+  },
+
+  _equate() {
+    store.newInput = eval(store.curInput);
+  },
+
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("section", { className: "button-set--equations" }, /*#__PURE__*/
+      React.createElement(Button, { text: "+", clickHandler: this._eq }), /*#__PURE__*/
+      React.createElement(Button, { text: "-", clickHandler: this._eq }), /*#__PURE__*/
+      React.createElement(Button, { text: "*", clickHandler: this._eq }), /*#__PURE__*/
+      React.createElement(Button, { text: "/", clickHandler: this._eq }), /*#__PURE__*/
+      React.createElement(Button, { text: "=", clickHandler: this._equate })));
 
 
-    //reset scrollbar position after refresh
-    window.onbeforeunload = function() {
-        window.scrollTo(0, 0);
+  } });
+
+const ButtonSetNumbers = React.createClass({ displayName: "ButtonSetNumbers",
+  _number(num) {
+    if (!store.curInput) {
+      return store.newInput = num;
     }
 
+    return store.newInput = `${store.curInput}${num}`;
+  },
 
-let fullscreen;
-let fsEnter = document.getElementById('fullscr');
-fsEnter.addEventListener('click', function (e) {
-    e.preventDefault();
-    if (!fullscreen) {
-        fullscreen = true;
-        document.documentElement.requestFullscreen();
-        fsEnter.innerHTML = "Exit Fullscreen";
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("section", { className: "button-set--numbers" }, /*#__PURE__*/
+      React.createElement(Button, { text: "1", clickHandler: this._number }), /*#__PURE__*/
+      React.createElement(Button, { text: "2", clickHandler: this._number }), /*#__PURE__*/
+      React.createElement(Button, { text: "3", clickHandler: this._number }), /*#__PURE__*/
+      React.createElement(Button, { text: "4", clickHandler: this._number }), /*#__PURE__*/
+      React.createElement(Button, { text: "5", clickHandler: this._number }), /*#__PURE__*/
+      React.createElement(Button, { text: "6", clickHandler: this._number }), /*#__PURE__*/
+      React.createElement(Button, { text: "7", clickHandler: this._number }), /*#__PURE__*/
+      React.createElement(Button, { text: "8", clickHandler: this._number }), /*#__PURE__*/
+      React.createElement(Button, { text: "9", clickHandler: this._number }), /*#__PURE__*/
+      React.createElement(Button, { text: "0", clickHandler: this._number })));
+
+
+  } });
+
+
+let store = {
+  input: 0,
+  memory: [],
+  get curInput() {
+    return this.input;
+  },
+
+  get curMemories() {
+    return this.memory.filter(m => m !== undefined);
+  },
+
+  set commitMemory(input) {
+    this.memory.push(input);
+  },
+
+  set newInput(str) {
+    let curInput = str,
+    oldInput = this.curInput;
+
+    if (this.curMemories.indexOf(oldInput) === -1) {
+      this.commitMemory = oldInput;
     }
-    else {
-        fullscreen = false;
-        document.exitFullscreen();
-        fsEnter.innerHTML = "Go Fullscreen";
-    }
-});
+
+    this.input = curInput;
+    ee.emitEvent('numberCruncher', [this.curInput]);
+  } };
+
+
+React.render( /*#__PURE__*/
+React.createElement(App, null),
+document.querySelector('body'));
